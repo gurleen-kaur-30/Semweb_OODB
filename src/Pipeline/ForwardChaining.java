e489ea8c2dfbc7c6a0116febf604674deb9ce833
prateksha <pratekshau@gmail.com>
2021-04-25 18:09:13 +0530

    Reasoner

--- src/Pipeline/ForwardChaining.java
+++ src/Pipeline/ForwardChaining.java
@@ -7,7 +7,12 @@ public class ForwardChaining {
 	
 	public static void main(String[] argv) {
 		String ontologyURI = "http://www.semanticweb.org/prateksha/ontologies/2021/1/college";
-		OWLModelManager owlModel = ProtegeOWL.createJenaOWLModelFromURI(ontologyURI);
+	}
+	public ProtegeReasoner createPelletOWLAPIReasoner(OWLModel owlModel) {
+		// Get the reasoner manager instance 
+		ReasonerManager reasonerManager = ReasonerManager.getInstance(); 
+		//Get an instance of the Protege Pellet reasoner 
+		ProtegeReasoner reasoner = reasonerManager.createProtegeReasoner(owlModel, ProtegePelletOWLAPIReasoner.class); return reasoner; }
 	}
 	
 	public static void inverseOfProperty() {

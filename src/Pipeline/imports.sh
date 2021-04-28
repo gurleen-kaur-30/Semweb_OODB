0963f9e0a5a3ce1de5a13a12760d059f8222806d
prateksha <pratekshau@gmail.com>
2021-04-28 11:51:44 +0530

    Updated imports script

--- src/Pipeline/imports.sh
+++ src/Pipeline/imports.sh
@@ -4,14 +4,14 @@ CLASSES_DIR_2=../ProtegeGenCode/$PROJECT_NAME/impl
 
 for c in $CLASSES_DIR_1/*; 
 do 
+    echo "Updating $c"
     sed -i "s/package $PROJECT_NAME/package ProtegeGenCode.$PROJECT_NAME/g" "$c"
-    sed -i "s/import $PROJECT_NAME.impl.*/import ProtegeGenCode.$PROJECT_NAME.impl.*/g" "c"
+    sed -i "s/import $PROJECT_NAME.impl.*/import ProtegeGenCode.$PROJECT_NAME.impl.*;/g" "$c"
 done
 
 for c in $CLASSES_DIR_2/*; 
 do 
-    echo "$fn"
-    sed -i "s/package $PROJECT_NAME.impl/package ProtegeGenCode.$PROJECT_NAME/g" "$c"
-    sed -i "s/import $PROJECT_NAME.*/import ProtegeGenCode.$PROJECT_NAME.*\n\nimport java.util.HashSet;\n\nimport java.io.Serializable;\nimport javax.jdo.annotations.Embedded;\nimport javax.persistence.Entity;\nimport javax.persistence.GeneratedValue;\nimport javax.persistence.Id;\n/g" "$c"
+    echo "Updating $c"
+    sed -i "s/package $PROJECT_NAME.impl/package ProtegeGenCode.$PROJECT_NAME.impl/g" "$c"
+    sed -i "s/import $PROJECT_NAME.*/import ProtegeGenCode.$PROJECT_NAME.*;\n\nimport java.util.HashSet;\n\nimport java.io.Serializable;\nimport javax.jdo.annotations.Embedded;\nimport javax.persistence.Entity;\nimport javax.persistence.GeneratedValue;\nimport javax.persistence.Id;\n/g" "$c"
 done
-

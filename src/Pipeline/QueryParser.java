7e1149e081c0037a368ed7654d5428c4db1f9421
Lakshay-agarwal <lakshayagarwal0@gmail.com>
2021-05-16 17:47:50 +0530

    Changes to QueryParser

--- src/Pipeline/QueryParser.java
+++ src/Pipeline/QueryParser.java
@@ -17,7 +17,7 @@ public class QueryParser {
             // System.out.println(m.group(1));
         }
         
-        String delims = "[ .,?!]+";
+        String delims = "[ .,!]+";
         String[] list = res.get(0).split(delims);
 
         // for(String t : list)
@@ -27,10 +27,15 @@ public class QueryParser {
     }
 
     public static String convertQuery(String s) {
-        String jpqlQuery = "";
         String[] select;
         String[] where; 
+        
+        String jpqlQuery = "";
+        String jpqlSelect = "";
         String jpqlFrom = "ProtegeGenCode.CollegeProtege.impl.Default";
+        String jpqlWhere = "";
+
+        int flag = 0;
 
         Pattern pattern = Pattern.compile("#(.*?)>");
         jpqlFrom += matchPattern(s, pattern)[0];
@@ -38,17 +43,30 @@ public class QueryParser {
     
         pattern = Pattern.compile("\\{([^}]+)\\}");
         where = matchPattern(s, pattern);
+        where = Arrays.copyOfRange(where, 1, where.length);
 
         for(String t : where)
             System.out.println(t);
         
         System.out.println("");
+        
+        pattern = Pattern.compile("[?]");
+
+        if(!pattern.matcher(where[2]).find()) {
+
+            pattern = Pattern.compile("[:]");
+
+            if(pattern.matcher(where[1]).find()) {
+
+                jpqlWhere = " where s." + where[1].split(":")[1] + " = " + where[2];
+            }
+        }
 
         pattern = Pattern.compile("SELECT(.+?)WHERE");
         select = matchPattern(s, pattern);
         select = Arrays.copyOfRange(select, 1, select.length);
 
-        String jpqlSelect = "";
+        int i = 0;
 
         for(String t : select) {
 
@@ -56,10 +74,10 @@ public class QueryParser {
 
             if(t.equals("*"))
                 jpqlSelect += t;
-            else if(t.equals(where[1]))
-                jpqlSelect += "s.name" + " ";
-            else if(t.equals(where[3])){
-                String[] temp = where[2].split("[:]");
+            else if(t.equals(where[0]))
+                jpqlSelect += "s.name";
+            else if(t.equals(where[i])){
+                String[] temp = where[1].split("[:]");
                 jpqlSelect += "s." + temp[1]; 
             } 
             else if(Pattern.compile("[?]").matcher(t).find()){
@@ -67,15 +85,16 @@ public class QueryParser {
             }
             else
                 jpqlSelect += "s." + t + " ";
+
+            jpqlSelect += " ";
+            i += 2;
             
         }
 
-        // System.out.println(jpqlSelect);
-
         if(jpqlSelect.equals("*"))
-            jpqlQuery = "select " + jpqlSelect + " from " + jpqlFrom;
+            jpqlQuery = "select " + jpqlSelect + "from " + jpqlFrom;
         else
-            jpqlQuery = "select " + jpqlSelect + " from " + jpqlFrom + " s";
+            jpqlQuery = "select " + jpqlSelect + "from " + jpqlFrom + " s" + jpqlWhere;
         
         return jpqlQuery;
     }
@@ -84,6 +103,19 @@ public class QueryParser {
 		String s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject ?object WHERE { ?subject foo:Id ?object .}");
 		
         System.out.println(s);
+
+        s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject WHERE { ?subject foo:Id \"42\" .}");
+		
+        System.out.println(s);
+
+        s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?Id ?age WHERE { ?subject foo:Id ?Id . ?subject foo:age ?age .}");
+		
+        System.out.println(s);
+
+        s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?Id ?age WHERE { ?subject foo:Id ?Id ; foo:age ?age .}");
+		
+        System.out.println(s);
+
 	}
 
 }

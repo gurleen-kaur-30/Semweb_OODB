dad120e432fa11827e8eef48ec0f8b569b0701ad
prateksha <pratekshau@gmail.com>
2021-05-18 13:57:42 +0530

    Added more queries

--- src/Pipeline/QueryParser.java
+++ src/Pipeline/QueryParser.java
@@ -205,17 +205,20 @@ public class QueryParser {
 
 
     public static void main(String[] argv) {
-    	String query = "PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject WHERE { ?subject rdf:type foo: }";
+//    	String query = "PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject WHERE { ?subject rdf:type foo: }";
 //    	String query = "PREFIX foo: <http://www.semanticweb.org/gurleen/ontologies/people#people> SELECT ?subject WHERE { ?subject rdf:type foo: }";
-    	int type = 1;
+//    	int type = 1;
     	
     	
-//    	String query = "PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#> PREFIX foo1: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject ?object WHERE{ ?subject foo:hasDebt ?object . ?subject rdf:type foo1: .}";
-//		String query = "PREFIX foo: <http://www.semanticweb.org/gurleen/ontologies/people#> PREFIX foo1: <http://www.semanticweb.org/gurleen/ontologies/people#people> SELECT  ?subject  ?object WHERE{ ?subject foo:hasFriend ?object . ?subject rdf:type foo1: .}";	
-//    	int type = 2;
+    	String query = "PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#> PREFIX foo1: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject ?object WHERE{ ?subject foo:hasRollNumber ?object . ?subject rdf:type foo1: .}";
+//		String query = "PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#> PREFIX foo1: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#Professor> SELECT ?subject ?object WHERE{ ?subject foo:teaches ?object . ?subject rdf:type foo1: .}";
+//    	String query = "PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#> PREFIX foo1: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject ?object WHERE{ ?subject foo:hasDebt ?object . FILTER (?object < 3000) ?subject rdf:type foo1: .}";
+
+//    	String query = "PREFIX foo: <http://www.semanticweb.org/gurleen/ontologies/people#> PREFIX foo1: <http://www.semanticweb.org/gurleen/ontologies/people#people> SELECT  ?subject  ?object WHERE{ ?subject foo:hasFriend ?object . ?subject rdf:type foo1: .}";	
+    	int type = 2;
 		
-//        String db_path = "jars/db/college.odb";
-    	String db_path = "jars/db/people.odb";
+        String db_path = "jars/db/college.odb";
+//    	String db_path = "jars/db/people.odb";
         EntityManagerFactory emf = Persistence.createEntityManagerFactory(db_path);
         EntityManager em = emf.createEntityManager();
         RunQuery rq = new RunQuery();
@@ -233,31 +236,6 @@ public class QueryParser {
     	
     	em.close();
     	emf.close();
-    	// String s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject ?object WHERE { ?subject foo:Id ?object .}");		
-        // System.out.println(s);
-
-        // s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject WHERE { ?subject foo:Id \"42\" .}");		
-        // System.out.println(s);
-
-        // s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?Id ?age WHERE { ?subject foo:Id ?Id . ?subject foo:age ?age .}");		
-        // System.out.println(s);
-
-        // s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?Id ?age WHERE { ?subject foo:Id ?Id ; foo:age ?age .}");
-        // System.out.println(s);
-
-//        String s = convertQuery("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject WHERE { ?subject rdf:type foo:} ");
-//        System.out.println(s);
-//
-//        s = convertQuery2("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#> PREFIX foo1: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject ?object WHERE{ ?subject foo:hasRollNumber ?object . ?subject rdf:type foo1: .}");
-//        System.out.println(s);
-//
-//        s = convertQuery2("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#> PREFIX foo1: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#Professor> SELECT ?subject ?object WHERE{ ?subject foo:teaches ?object . ?subject rdf:type foo1: .}");
-//        System.out.println(s);
-//
-//        s = convertQuery2("PREFIX foo: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#> PREFIX foo1: <http://www.semanticweb.org/prateksha/ontologies/2021/1/college#iMTech> SELECT ?subject ?object WHERE{ ?subject foo:hasDebt ?object . FILTER (?object < 3000) ?subject rdf:type foo1: .}");
-//        System.out.println(s);
-
-
 	}
 
 }
